7d08e16e38cdb60bc86a454796a860b9bfdfcb71
Dirk Elmendorf <dirk@r26d.com>
2020-03-16 19:24:47 -0500

    Patch

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,11 @@ set -e
 #Confirm that there is a Digital Ocean Access Token
 #Confirm that there is a cluster name
 if [[ -z "${DIGITALOCEAN_ACCESS_TOKEN}" ]]; then
-  echo "Token Found"
-  else
     echo "DIGITALOCEAN_ACCESS_TOKEN missing!"
     exit 127
 fi
 if [[ -z "${DIGITALOCEAN_K8S_CLUSTER_NAME}" ]]; then
-  echo "Cluster Name Found"
-  else
-    echo "DIGITALOCEAN_K8S_CLUSTER_NAME missing!"
+     echo "DIGITALOCEAN_K8S_CLUSTER_NAME missing!"
     exit 127
 fi
 doctl auth init -t ${DIGITALOCEAN_ACCESS_TOKEN}
