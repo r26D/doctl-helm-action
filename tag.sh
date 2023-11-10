8be0bfc726d24e34a40e3ddca95aa02f1af3665b
Dirk Elmendorf <dirk@r26d.com>
2023-11-10 14:20:39 -0600

    Fixing typo

--- tag.sh
+++ tag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i  "s/doctl-helm-action:v.*/doctl-helm-action:v${2}/" action.yml
+sed -i  "s/doctl-helm-action:v.*/doctl-helm-action:v${2}\"/" action.yml
 git commit -a -m "${1}"
 git tag -a -m "${1}" "v${2}"
 git push --follow-tags
